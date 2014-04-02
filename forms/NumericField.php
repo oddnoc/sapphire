41976a870af62ede6efebaae5debfc854f22473d
Damian Mooyman <damian@silverstripe.com>
2014-04-03 09:33:18 +1300

    BUG Fixed handling of numbers in certain locales. Fixes #2161

--- forms/NumericField.php
+++ forms/NumericField.php
@@ -9,6 +9,35 @@
  */
 class NumericField extends TextField {
 
+	public function setValue($value) {
+		require_once "Zend/Locale/Format.php";
+		if (is_numeric($value)) {
+			// Converts the value of an input from a locale specific number format
+			$locale = new Zend_Locale(i18n::get_locale());
+			$this->value = Zend_Locale_Format::toNumber($value, array('locale' => $locale));
+		} else {
+			// If an invalid number, store it anyway, but validate() will fail
+			$this->value = trim($value);
+		}
+		return $this;
+	}
+	
+	/**
+	 * Determine if the current value is a valid number in the current locale
+	 * 
+	 * @param mixed $value Optional value to check against. Will check the current value if not given
+	 * @return bool
+	 */
+	public function IsNumeric($value = null) {
+		if(!func_num_args()) $value = $this->value;
+		require_once "Zend/Locale/Format.php";
+		$locale = new Zend_Locale(i18n::get_locale());
+		return Zend_Locale_Format::isNumber(
+			trim($this->value),
+			array('locale' => $locale)
+		);
+	}
+
 	public function Type() {
 		return 'numeric text';
 	}
@@ -18,30 +47,29 @@ class NumericField extends TextField {
 			return true;
 		}
 		
-		require_once THIRDPARTY_PATH."/Zend/Locale/Format.php";
+		if($this->IsNumeric()) return true;
 
-		$valid = Zend_Locale_Format::isNumber(
-			trim($this->value), 
-			array('locale' => i18n::get_locale())
+		$validator->validationError(
+			$this->name,
+			_t(
+				'NumericField.VALIDATION', "'{value}' is not a number, only numbers can be accepted for this field",
+				array('value' => $this->value)
+			),
+			"validation"
 		);
-
-		if(!$valid) {
-			$validator->validationError(
-				$this->name,
-				_t(
-					'NumericField.VALIDATION', "'{value}' is not a number, only numbers can be accepted for this field",
-					array('value' => $this->value)
-				),
-				"validation"
-			);
-
-			return false;
-		}
-		
-		return true;
+		return false;
 	}
-	
+
+	/**
+	 * Extracts the number value from the localised string value
+	 * 
+	 * @return string number value
+	 */
 	public function dataValue() {
-		return (is_numeric($this->value)) ? $this->value : 0;
+		require_once "Zend/Locale/Format.php";
+		if(!$this->IsNumeric()) return 0;
+		$locale = new Zend_Locale(i18n::get_locale());
+		$number = Zend_Locale_Format::getNumber($this->value, array('locale' => $locale));
+		return $number;
 	}
 }
--- tests/forms/NumericFieldTest.php
+++ tests/forms/NumericFieldTest.php
@@ -16,22 +16,62 @@ class NumericFieldTest extends SapphireTest {
 
 		$validator = new RequiredFields('Number');
 		$this->assertTrue($field->validate($validator));
+		$this->assertEquals(12.0, $field->dataValue());
 
 		$field->setValue('12,00');
 		$this->assertFalse($field->validate($validator));
 
+		// Treats '0' as given for the sake of required fields
 		$field->setValue('0');
 		$this->assertTrue($field->validate($validator));
+		$this->assertEquals(0, $field->dataValue());
+
+		// Should fail the 'required but not given' test
+		$field->setValue('');
+		$this->assertFalse($field->validate($validator));
 
 		$field->setValue(false);
 		$this->assertFalse($field->validate($validator));
-
+		
+		// Test german locale
 		i18n::set_locale('de_DE');
 		$field->setValue('12,00');
 		$validator = new RequiredFields();
 		$this->assertTrue($field->validate($validator));
+		$this->assertEquals('12,00', $field->Value());
+		$this->assertEquals(12.0, $field->dataValue());
 
+		// Test forgiveness tolerance
 		$field->setValue('12.00');
-		$this->assertFalse($field->validate($validator));
+		$this->assertTrue($field->validate($validator));
+		$this->assertEquals('12,00', $field->Value()); // converts decimal to comma
+		$this->assertEquals(12.0, $field->dataValue());
+
+		// Test finish locale
+		i18n::set_locale('fi_FI');
+		$field->setValue('12,00');
+		$validator = new RequiredFields();
+		$this->assertTrue($field->validate($validator));
+		$this->assertEquals('12,00', $field->Value());
+		$this->assertEquals(12.0, $field->dataValue());
+		
+		// Thousands separator
+		$field->setValue('21 212,00');
+		$validator = new RequiredFields();
+		$this->assertTrue($field->validate($validator));
+		$this->assertEquals('21 212,00', $field->Value());
+		$this->assertEquals(21212.0, $field->dataValue());
+
+		// Test forgiveness tolerance
+		$field->setValue('12.00');
+		$validator = new RequiredFields();
+		$this->assertTrue($field->validate($validator));
+		$this->assertEquals('12,00', $field->Value()); // converts decimal to comma
+		$this->assertEquals(12.0, $field->dataValue());
+
+		$field->setValue('21212,00');
+		$validator = new RequiredFields();
+		$this->assertTrue($field->validate($validator));
+		$this->assertEquals(21212.0, $field->dataValue());
 	}
 }
